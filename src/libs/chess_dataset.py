7377374b3e1e2c19c091cb52945ae4c58021d9cf
Jerome Etienne <jerome.etienne@gmail.com>
2025-09-25 16:20:04 +0200

    Refactor ChessDataset class: rename parameters for clarity and consistency

--- src/libs/chess_dataset.py
+++ src/libs/chess_dataset.py
@@ -1,14 +1,18 @@
+# pip imports
+import torch
+
+# local imports
 from torch.utils.data import Dataset
 
 
 class ChessDataset(Dataset):
-    def __init__(self, X, y):
-        self.X = X
-        self.y = y
+    def __init__(self, boards_tensor: torch.Tensor, moves_tensor: torch.Tensor):
+        self.boards_tensor = boards_tensor
+        self.moves_tensor = moves_tensor
 
     def __len__(self):
-        return len(self.X)
+        return len(self.boards_tensor)
 
     def __getitem__(self, idx):
-        return self.X[idx], self.y[idx]
+        return self.boards_tensor[idx], self.moves_tensor[idx]
 
