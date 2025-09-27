17c68e81129f6565af6ab16503d18ed49e5c5c9c
Jerome Etienne <jerome.etienne@gmail.com>
2025-09-27 20:16:15 +0200

    Refactor EarlyStopper initialization and early stop logic for improved clarity and functionality

--- src/libs/early_stopper.py
+++ src/libs/early_stopper.py
@@ -1,5 +1,5 @@
 class EarlyStopper:
-    def __init__(self, patience:int=1, min_delta:float=0):
+    def __init__(self, patience: int = 10, min_delta: float = 0.001):
         """
         Early stops the training if validation loss doesn't improve after a given patience.
 
@@ -9,13 +9,13 @@ class EarlyStopper:
         """
         self.patience = patience
         self.min_delta = min_delta
-        self.counter = 0
-        self.min_validation_loss = float('inf')
+        self.wait_counter = 0
+        self.min_validation_loss = float("inf")
 
     def early_stop(self, validation_loss: float) -> tuple[bool, bool]:
         """
         Checks if training should be stopped early based on validation loss.
-        
+
         Args:
             - validation_loss (float): Current validation loss.
 
@@ -25,13 +25,18 @@ class EarlyStopper:
         """
         must_stop = False
         must_save = False
-        if validation_loss < self.min_validation_loss:
+
+        is_improved = validation_loss < (self.min_validation_loss - self.min_delta)
+
+        # check if the model MUST be saved
+        if is_improved:
             self.min_validation_loss = validation_loss
-            self.counter = 0
+            self.wait_counter = 0
             must_save = True
-        elif validation_loss > (self.min_validation_loss + self.min_delta):
-            self.counter += 1
-            if self.counter >= self.patience:
+        # check if the learning MUST be stopped
+        else:
+            self.wait_counter += 1
+            if self.wait_counter >= self.patience:
                 must_stop = True
 
         return (must_stop, must_save)
