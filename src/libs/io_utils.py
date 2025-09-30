b62ba7be1ebb1af406448ffb1a231f20bc13bcab
Jerome Etienne <jerome.etienne@gmail.com>
2025-10-01 00:17:20 +0200

    Add JSON handling for UCI to class mapping and deprecate old methods

--- src/libs/io_utils.py
+++ src/libs/io_utils.py
@@ -1,7 +1,14 @@
+import json
+from typing_extensions import deprecated
 from .chess_model import ChessModel
 import torch
 import pickle
 import os
+import chess
+
+
+__dirname__ = os.path.dirname(os.path.abspath(__file__))
+data_folder_path = os.path.join(__dirname__, "../../data")
 
 
 class IOUtils:
@@ -13,7 +20,7 @@ class IOUtils:
         torch.save(model.state_dict(), state_dict_path)
 
     @staticmethod
-    def load_model(folder_path: str, input_shape: tuple[int,int,int], output_shape: tuple[int]) -> ChessModel:
+    def load_model(folder_path: str, input_shape: tuple[int, int, int], output_shape: tuple[int]) -> ChessModel:
         # Load the model
         model = ChessModel(input_shape=input_shape, output_shape=output_shape)
         model_path = f"{folder_path}/model.pth"
@@ -43,7 +50,7 @@ class IOUtils:
         moves_tensor = torch.load(moves_path)
         with open(uci_to_classindex_path, "rb") as file:
             uci_to_classindex = pickle.load(file)
-        
+
         # return the dataset
         return boards_tensor, moves_tensor, uci_to_classindex
 
@@ -56,6 +63,7 @@ class IOUtils:
         return dataset_exists
 
     @staticmethod
+    @deprecated("Use uci2class_load instead")
     def load_uci_to_classindex(folder_path: str) -> dict[str, int]:
         """
         Load the uci_to_classindex mapping from a pickle file.
@@ -69,6 +77,25 @@ class IOUtils:
         return uci_to_classindex
 
     @staticmethod
+    @deprecated("Use uci2class_inverse_mapping instead")
     def classindex_to_uci_inverse_mapping(uci_to_classindex: dict[str, int]) -> dict[int, str]:
         classindex_to_uci = {v: k for k, v in uci_to_classindex.items()}
         return classindex_to_uci
+
+    @staticmethod
+    def uci2class_load(chess_color: chess.Color = chess.WHITE) -> dict[str, int]:
+        color_name = "white" if chess_color == chess.WHITE else "black"
+        file_path = os.path.join(data_folder_path, f"uci2class_arr_{color_name}.json")
+        with open(file_path, "r") as file_reader:
+            uci2class_arr: list[str] = json.load(file_reader)
+
+        # Build the mapping
+        uci2class = {move_uci: index for index, move_uci in enumerate(uci2class_arr)}
+
+        return uci2class
+
+    @staticmethod
+    def uci2class_inverse_mapping(chess_color: chess.Color = chess.WHITE) -> dict[int, str]:
+        uci_to_class = IOUtils.uci2class_load(chess_color)
+        class_to_uci = {value: key for key, value in uci_to_class.items()}
+        return class_to_uci
