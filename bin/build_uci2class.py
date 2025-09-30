6425a1726698247ccd200563842a887bc5f3e39e
Jerome Etienne <jerome.etienne@gmail.com>
2025-10-01 00:10:48 +0200

    Implement code changes to enhance functionality and improve performance

--- bin/build_uci2class.py
+++ bin/build_uci2class.py
@@ -2,13 +2,16 @@
 
 # stdlib imports
 import os
-import pickle
+import json
+
+# pip imports
+import chess
 
 # local imports
 from src.libs.chess_extra import ChessExtra
 
 __dirname__ = os.path.dirname(os.path.abspath(__file__))
-data_folder_path = os.path.abspath(os.path.join(__dirname__, "..", "output"))
+data_folder_path = os.path.abspath(os.path.join(__dirname__, "..", "data"))
 
 if __name__ == "__main__":
     all_moves_uci = ChessExtra.all_unique_moves()
@@ -17,13 +20,30 @@ if __name__ == "__main__":
     all_moves_uci = sorted(all_moves_uci)
 
     # Create uci2class mapping
-    uci2class = {move: idx for idx, move in enumerate(all_moves_uci)}
+    uci2class_white = [move_uci for move_uci in all_moves_uci]
     
     # Save the mapping to a file 
-    file_path = os.path.join(data_folder_path, "uci_to_classindex.pickle")
-    with open(file_path, "wb") as file_writer:
-        pickle.dump(uci2class, file_writer)
+    white_file_path = os.path.join(data_folder_path, "uci2class_arr_white.json")
+    with open(white_file_path, "w") as file_writer:
+        json.dump(uci2class_white, file_writer, indent=4)
+    print(f"Saved {len(uci2class_white)} unique moves to {white_file_path}")
 
-    print(f"Saved {len(uci2class)} unique moves to {file_path}")
 
-    
+    # Create uci2class mapping for black by mirroring XY the white moves
+    uci2class_black = []
+    for move_uci in uci2class_white:
+        white_move = chess.Move.from_uci(move_uci)
+
+        # Mirror the ranks and files for black
+        black_from_square = chess.square(7 - chess.square_file(white_move.from_square), 7 - chess.square_rank(white_move.from_square))
+        black_to_square = chess.square(7 - chess.square_file(white_move.to_square), 7 - chess.square_rank(white_move.to_square))
+        black_move = chess.Move(from_square=black_from_square, to_square=black_to_square, promotion=white_move.promotion)
+
+        black_move_uci = black_move.uci()
+        uci2class_black.append(black_move_uci)
+
+    # Save the mapping to a file 
+    black_file_path = os.path.join(data_folder_path, "uci2class_arr_black.json")
+    with open(black_file_path, "w") as file_writer:
+        json.dump(uci2class_black, file_writer, indent=4)
+    print(f"Saved {len(uci2class_black)} unique moves to {black_file_path}")    
